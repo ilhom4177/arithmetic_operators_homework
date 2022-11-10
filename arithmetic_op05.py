3ca2a9033bd4f2e2557c056dbffe78cbbf502dd7
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-10 22:16:23 +0500

    5 misol ishlandi

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' with data type int.
-
+number = int(3)
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-
+answer = number%10
 #Print 'answer'.
+print(answer)
